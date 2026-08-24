5eb583f0254afd64953c01ee09c31e3b78901fd4
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-24 23:32:17 +0530

    Working on lambda function.

--- lambda_intro.py
+++ lambda_intro.py
@@ -1,3 +1,11 @@
-nums = [(lambda: x) () for x in range(10)]
+# Lambda functions are small, anonymous functions defined using the lambda keyword.
+# They can take any number of arguments but contain only a single expression.
+# Lambda functions are useful for short, simple operations.
 
-print(nums)
+# def double(x):
+#     return x*2
+
+# result = double(2)
+double = lambda x: x * 2
+
+print(double(4))
