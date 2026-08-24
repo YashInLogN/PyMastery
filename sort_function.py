988f4586a6263a721557ec036e3cd96cfc171a65
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-24 23:43:02 +0530

    Created and working on sort.py

---
+++ sort_function.py
@@ -0,0 +1,4 @@
+# The sort() function is used to arrange the elements of a list in ascending order.
+# It can also sort elements in descending order using the reverse=True argument.
+# sort() changes the original list directly.
+
