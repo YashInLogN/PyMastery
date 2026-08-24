f675f8ac6a4a37537ba3e9fceafc092452f85076
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-24 23:58:53 +0530

    Using key in sorted.

--- sort_function.py
+++ sort_function.py
@@ -4,7 +4,17 @@
 
 students = ["Squidward", "Sandy", "Patrick", "Spongebob", "Mr .Krabs"]
 
-students.sort(reverse=True)
+# students.sort()
 
-for i in students:
+sorted_students = sorted(students)
+
+for i in sorted_students:
+    print(i)
+
+students_data = [("Henry", "F", 100), ("Clark", "C", 72), ("Hamilton", "A", 43)]
+
+grade = lambda grade: grade[1]
+sorted_data = sorted(students_data, key=grade)
+
+for i in sorted_data:
     print(i)
