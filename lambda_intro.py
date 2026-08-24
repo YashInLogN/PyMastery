10394de22edc747c3c434272a05ee340f7acb1e6
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-24 23:39:50 +0530

    Saved lambda.py

--- lambda_intro.py
+++ lambda_intro.py
@@ -7,5 +7,13 @@
 
 # result = double(2)
 double = lambda x: x * 2
+multiply = lambda x, y: x * y
+add = lambda x, y, z: x + y+ z
+full_name = lambda first_name, last_name: first_name + " " + last_name
+age_check = lambda x: True if x >= 18 else False
 
-print(double(4))
+print("Double: ", double(4))
+print("Multiply: ", multiply(3, 4))
+print("Addition: ", add(3, 4, 5))
+print("Full_name: ", full_name(first_name:= input("First_name: "), last_name:= input("last_name: ")))
+print("Age_check: ", age_check(age := int(input("Enter your age: "))))
