2d15defc7326a96dfe1b8b33951f1d5af19e21ff
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-25 18:14:14 +0530

    Done with list and dict comprehensions.

--- useful_builtins/dic_comprehension.py
+++ useful_builtins/dic_comprehension.py
@@ -3,7 +3,28 @@
 # Syntax: {key: value for item in iterable if condition}
 
 cities_in_F = {'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+print(f"Cities and temperatures(F)--->\n{cities_in_F}")
+# cities_F_sorted = dict(sorted(cities_in_F.items(), key= lambda item: item[1]))
+# print(cities_F_sorted)
 
-cities_f_sorted = dict(sorted(cities_in_F.items(), key= lambda item: item[1]))
+print('\n')
 
-print(cities_f_sorted)
+cities_in_C = {key: ((value-32)*(5/9)) for (key, value) in cities_in_F.items()}
+print(f"Cities and temperatures(C)--->\n{cities_in_F}")
+
+# cities_weather = {'New York': "snowing", 'Boston': "sunny", 'Los Angeles': "sunny", 'Chicago': "cloudy"}
+# check_cities_weather = {key: "Successfully" for (key,value) in cities_weather.items() if value=="snowing"}
+# print(check_cities_weather)
+
+def check_temp(v):
+    if v > 70:
+        return "HOT"
+    elif 50 <= v < 70:
+        return "WARM"
+    else:
+        return "COLD"
+
+# filter_temperature = {key: ("WARM" if value > 50 else "COLD") for (key, value) in cities_in_F.items()}
+filter_temperature = {key: check_temp(value) for (key, value) in cities_in_F.items()}
+
+print(filter_temperature)
