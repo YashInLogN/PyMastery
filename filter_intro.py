6133d7b9c9e877056cdf80e0bae09a1549f37fb6
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-25 13:17:57 +0530

    Working on filter.py

--- filter_intro.py
+++ filter_intro.py
@@ -2,3 +2,28 @@
 # It returns a filter object containing only the elements that satisfy the condition.
 # Parameters required: filter(function, iterable)
 # function → condition to check; iterable → collection of elements to filter.
+
+people = [
+    ("Nakul", 18),
+    ("Aman", 20),
+    ("Rahul", 19),
+    ("Priya", 21),
+    ("Arjun", 22)
+]
+
+check_age = lambda age: age[1] > 18
+
+with_age = list(filter(check_age, people))
+age = lambda age: age[1]
+sorted_ = sorted(with_age, key=age)
+
+for i in sorted_:
+    print(i)
+
+print("Ages after 2 years-----")
+
+age_later = lambda later: (later[0], later[1] + 2)
+with_new_age = list(map(age_later, sorted_))
+
+for i in with_new_age:
+    print(i)
