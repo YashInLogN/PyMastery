12e5a4916a8ceb1da388e08d3d0bc0c2ec566450
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-26 23:55:02 +0530

    Introducing time module.

---
+++ mod/time_module.py
@@ -0,0 +1,5 @@
+# The time module provides functions for working with time and time-related operations.
+# It can be used to get the current time, pause program execution, and measure time intervals.
+# Common functions include time(), sleep(), ctime(), and strftime().
+
+import time
