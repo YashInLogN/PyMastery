67fc0a10b26af9e4bba98d87c052af731ba3b21c
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-26 23:45:53 +0530

    Zip introduced.

---
+++ zip_function.py
@@ -0,0 +1,11 @@
+# The zip() function combines elements from two or more iterables into pairs or tuples.
+# It matches elements based on their positions and returns a zip object.
+# Syntax: zip(iterable1, iterable2, ...)
+
+names = ["Clark", "Alice", "Bob"]
+Origin = ["Argentina", "Turkey", "France"]
+
+data = dict(zip(names, Origin))
+
+for i in data.items():
+    print(i)
